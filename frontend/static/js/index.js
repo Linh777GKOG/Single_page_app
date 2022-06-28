85fe8d7ff7ba5e386d4f59a8d5b703e21a31558c
Linh Vu <linh777gkog@gmail.com>
2022-06-28 23:25:20 +0700

    test route

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -18,3 +18,40 @@ const getParams = (match) => {
     })
   );
 };
+
+const navigateTo = (url) => {
+  history.pushState(null, null, url);
+  router();
+};
+
+const router = async () => {
+  const routes = [
+    { path: '/', view: Dashboard },
+    { path: '/posts', view: Posts },
+    { path: '/posts/:id', view: PostView },
+    { path: '/settings', view: Settings },
+  ];
+
+  // Test each route for potential match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      result: location.pathname.match(pathToRegex(route.path)),
+    };
+  });
+
+  let match = potentialMatches.find(
+    (potentialMatch) => potentialMatch.result !== null
+  );
+
+  if (!match) {
+    match = {
+      route: routes[0],
+      result: [location.pathname],
+    };
+  }
+
+  const view = new match.route.view(getParams(match));
+
+  document.querySelector('#app').innerHTML = await view.getHtml();
+};
