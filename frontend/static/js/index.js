56c60aec726c4c44e20329f25090e7e38a2c70f8
Linh Vu <linh777gkog@gmail.com>
2022-06-28 23:24:52 +0700

    import js

---
+++ frontend/static/js/index.js
@@ -0,0 +1,20 @@
+import Dashboard from './views/Dashboard.js';
+import Posts from './views/Posts.js';
+import PostView from './views/PostView.js';
+import Settings from './views/Settings.js';
+
+const pathToRegex = (path) =>
+  new RegExp('^' + path.replace(/\//g, '\\/').replace(/:\w+/g, '(.+)') + '$');
+
+const getParams = (match) => {
+  const values = match.result.slice(1);
+  const keys = Array.from(match.route.path.matchAll(/:(\w+)/g)).map(
+    (result) => result[1]
+  );
+
+  return Object.fromEntries(
+    keys.map((key, i) => {
+      return [key, values[i]];
+    })
+  );
+};
